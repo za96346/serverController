01238952b2705ce939258152eb072853554889a1
siou <za96346@gmail.com>
2023-01-06 22:27:24 +0800

    fix

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!env -i /bin/bash --noprofile --norc
 CONTAINER=workapp_mysql
 DB_NAME=workApplication
 FILENAME=~/backup/workApplication/"$(date "+%Y%m%d_%H%M%S")".sql
 pwd=siou0722
+which python
 
 docker exec -i $CONTAINER bash -c "mysqldump -u root -p$pwd  $DB_NAME" > $FILENAME
 
-python ~/serverController/sendEmail/backupSuccess.py
+python ~/sendEmail/backupSuccess.py
