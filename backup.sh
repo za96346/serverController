16d809ca7cb5d7ba1339e9826d1e868b1fadc9dd
siou <za96346@gmail.com>
2023-01-06 22:02:25 +0800

    fix

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ DB_NAME=workApplication
 FILENAME=~/backup/workApplication/"$(date "+%Y%m%d_%H%M%S")".sql
 pwd=siou0722
 docker exec -i $CONTAINER bash -c "mysqldump -u root -p$pwd  $DB_NAME" > $FILENAME
-if [-f $FILENAME]
+if [-f "$FILENAME"]
 then
     python ./sendEmail/backupSuccess.py
 else
