5fea835bfc932b07796aaa0307f75e8629fe9193
siou <za96346@gmail.com>
2023-01-06 22:12:06 +0800

    fix

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@ DB_NAME=workApplication
 FILENAME=~/backup/workApplication/"$(date "+%Y%m%d_%H%M%S")".sql
 pwd=siou0722
 
+docker exec -i $CONTAINER bash -c "mysqldump -u root -p$pwd  $DB_NAME" > $FILENAME &
 PID=$!
-docker exec -i $CONTAINER bash -c "mysqldump -u root -p$pwd  $DB_NAME" > $FILENAME
 wait $PID
 if [-f "$FILENAME"]
 then
